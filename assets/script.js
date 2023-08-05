eda7fa57af239158dd8bda7c877b701129f82879
dewey preast <webwalkin@me.com>
2023-08-05 09:43:24 -0400

    made smaller change just getting window object started

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var questionprompt = document.getElementsByClassName("Question");
+var Question = document.getElementsByClassName("Question");
 var answer = {} ;
 var scores = "";
 var finalScore = {};
@@ -7,12 +7,12 @@ var incorrectAnswer = "";
 // var questionVarieties = ["Experience", "Resources", "Participation", "Preferences"]
 
 
-var questionArray = ["How long have you been gardening?", "Have you Saved Seeds Before?"];
+const questionArray = ["How long have you been gardening?", "Have you Saved Seeds Before?"];
 console.log(Question);
 
- function askQuestion(questionArray[1]) {
-    var prompt = window.alert();
-    console.log(questionArray[1]);
+ function askQuestion() {
+    var Question = window.prompt(questionArray[1]);
+    console.log();
 }
 
 
